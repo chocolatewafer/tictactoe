3be3ddb8ad5cb7de6a29521deb9643b45c48f0a4
Sparsh Nidhi <sparsh123nidhi@gmail.com>
2025-05-14 20:46:29 +0545

    test and logic for player move

--- app/api/routes/board.py
+++ app/api/routes/board.py
@@ -10,6 +10,19 @@ class Board(BaseModel):
     grid: list[list[str]]
 
 
+class player_move(BaseModel):
+    row: int
+    col: int
+
+
 @router.get("/")
 async def get_board():
     return game_board
+
+
+@router.post("/")
+async def put_board(move: player_move):
+    row = move.row
+    col = move.col
+    game_board["grid"][row][col] = "o"
+    return f"player o added to {row},{col}"
